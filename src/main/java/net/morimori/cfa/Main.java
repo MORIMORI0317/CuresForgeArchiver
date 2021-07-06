05ca2ca03f35f76747caa0449aace4babee783e0
MORIMORI0317 <morimori.0317@outlook.jp>
2021-07-06 09:59:13 +0900

    init

---
+++ src/main/java/net/morimori/cfa/Main.java
@@ -0,0 +1,10 @@
+package net.morimori.cfa;
+
+import com.therandomlabs.curseapi.minecraft.CurseAPIMinecraft;
+
+public class Main {
+    public static void main(String[] args) {
+        CurseAPIMinecraft.initialize();
+
+    }
+}
